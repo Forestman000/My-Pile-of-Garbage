015e11ea9495233da93097d3d9116de8bb9507f5
Forestman000 <connorpetts28@gmail.com>
2024-09-01 14:15:39 +1000

    Update main.py

--- Unipoint v1.0.0/main.py
+++ Unipoint v1.0.0/main.py
@@ -14,27 +14,51 @@ tempstring2 = ""
 symbol = 0
 charset = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz \n,.\'\":;(){}[]=+-*/<>`!?|~%#@$&_"
 varlist = []
+funclist = []
 add_string = "\033[36m"
 Econtent = ""
 
 for i in range(256):
     varlist.append("")
+for I in range(32):
+    funclist.append(['',''])
+#print(funclist)
 
 def error1():
-    print("\033[31mValue Error Found at symbol #", symbol, ". process terminated")
+    print("\033[31mValue Error Encountered at symbol #", symbol, ". process terminated")
+    quit()
+
+def error2():
+    print("\033[31mType Error Encountered at symbol #", symbol, ". process terminated")
+    quit()
+
+def error3():
+    print("\033[31mAttempt to assign character outside character value range Encountered at symbol #", symbol, ". process terminated")
     quit()
 
 
+def function(symbol, pointer):
+    pass
+
+
 while charnum < len(content):
-    if content[charnum] == "(":
+    if content[charnum] == "{":
+        charnum += 1
+        symbol += 1
+        while content[charnum] != "}":
+            charnum +=1
+    elif content[charnum] == "}":
+        charnum += 1
+        symbol += 1
+    elif content[charnum] == "(":
         charnum += 1
         symbol += 1
         charmode = True
-    if content[charnum] == ")":
+    elif content[charnum] == ")":
         charnum += 1
         symbol += 1
         charmode = False
-    if content[charnum] == "+":
+    elif content[charnum] == "+":
         pointer += 1
         charnum += 1
         symbol += 1
@@ -57,7 +81,7 @@ while charnum < len(content):
             if 1 <= pointer <= len(charset):
                 char = charset[pointer - 1]
             else:
-                char = '?'
+                error3()
             temparray.append(char)
             tempstring = "".join(temparray)
         elif charmode == False:
@@ -65,7 +89,7 @@ while charnum < len(content):
             temparray.append(varlist[pointer])
             print("variablised")
             print("t", tempstring)
-    elif content[charnum] == "|":
+    elif content[charnum] == "\\":
         charnum += 1
         symbol += 1
         tempstring2 = varlist[pointer]
@@ -86,16 +110,26 @@ while charnum < len(content):
         if pointer == 4:
             try:
                 tempstring.isdigit()
+                tempstring2.isdigit()
                 int(tempstring)
+                int(tempstring2)
+                print("inty")
             except ValueError:
                 error1()
         if pointer == 5:
-            ans = int(tempstring) + int(tempstring2)
+            try:
+                if type(tempstring, tempstring2) == int:
+                    ans = (tempstring) + (tempstring2)
+            except TypeError:
+                error2()
             print(tempstring)
             print(tempstring2)
             print("a =", ans)
         if pointer == 6:
-            ans = int(tempstring) - int(tempstring2)
+            try:
+                ans = int(tempstring) - int(tempstring2)
+            except ValueError:
+                error1()
             print(tempstring)
             print(tempstring2)
             print("a =", ans)
@@ -107,6 +141,20 @@ while charnum < len(content):
             print(tempstring)
             print(tempstring2)
             print("a =", ans)
+        try:
+            # Convert tempstring and tempstring2 to floats
+            operand1 = float(tempstring)
+            operand2 = float(tempstring2)
+
+            # Perform the division operation
+            ans = operand1 / operand2
+        except ValueError:
+            error1()
+        except ZeroDivisionError:
+            print("\033[31mError: Division by zero is not allowed.")
+            quit()
+
+        print("a =", ans)
         if pointer >= 8:
             varlist[pointer] = tempstring
             print("append", ":", tempstring)
